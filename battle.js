3c323f97501e30af16516dadae4b4f50a494a134
Rathief <rafie.fadlurahman@gmail.com>
2023-07-20 14:09:43 +0700

    commit up to register php

---
+++ battle.js
@@ -0,0 +1,11 @@
+const enemies = [
+    { name: "skeleton", hp: 5, att: 1},
+    { name: "demon", hp: 10, att: 3}
+]
+const levels = {
+    easy: [1,1,2],
+    hard: [2,2,2]
+}
+function battle(levelName){
+    let level = levels[levelName]
+}
---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * using mysqli_connect for database connection
+ */
+ 
+$databaseHost = 'localhost';
+$databaseName = 'milestoneproject';
+$databaseUsername = 'root';
+$databasePassword = '';
+ 
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+ 
+if ($mysqli->connect_error) {
+    die("Koneksi gagal: " . $mysqli->connect_error);
+}
+
+echo "Koneksi berhasil";
+?>
